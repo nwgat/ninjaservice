444198789498452f6446329fe5be286c14553f55
nwgat <github@nwgat.net>
2016-04-07 21:03:41 +0200

    d
    d

---
+++ ninjaservice.py
@@ -0,0 +1,24 @@
+# nwgat.ninja
+import os
+import subprocess
+import schedule
+import arrow
+from time import sleep
+
+def job():
+    utc = arrow.utcnow()
+    output = subprocess.check_output(["ps", "x"]);  # linux
+#   output = subprocess.check_output(["tasklist"]); # windows
+    if "lftp\b" in output:
+         print 'lftp is already running'
+    else:
+         cmd = 'lftp -f test.lftp'
+         os.system(cmd)
+
+schedule.every(9).seconds.do(job)
+#schedule.every().hour.do(job)
+#schedule.every().day.at("10:30").do(job)
+
+while True:
+    schedule.run_pending()
+    sleep(1)
