b7eeb911310ac26c7353acd6295218a94317f712
nwgat <github@nwgat.net>
2016-04-08 13:16:49 +0200

    Update ninjaservice.py

--- ninjaservice.py
+++ ninjaservice.py
@@ -12,11 +12,12 @@ def job():
     if "lftp\b" in output:
          print 'lftp is already running'
     else:
-         cmd = 'lftp -f test.lftp'
+         cmd = 'lftp -f /home/user/script' # linux
+#         cmd = 'bash.exe -c "/usr/bin/lftp.exe -f /cygdrive/d/sb/bin/tv.lftp"' #windows
          os.system(cmd)
 
-schedule.every(9).seconds.do(job)
-#schedule.every().hour.do(job)
+#schedule.every(9).seconds.do(job)
+schedule.every().hour.do(job)
 #schedule.every().day.at("10:30").do(job)
 
 while True:
